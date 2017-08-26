70345dca73b970f9313d8b8fe8c8314965564391
iyeongjun <they6687@naver.com>
2017-08-26 14:15:07 +0900

    Add GameObject,Script,Images

---
+++ unity-clicker-game/Assets/Script/DataController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataController : MonoBehaviour
+{
+	private static GameObject _container;
+
+	public static GameObject Container
+	{
+		get { return _container; }
+	}
+
+	private static DataController _instance;
+
+	public static DataController Instance
+	{
+		get
+		{
+			if (!_instance)
+			{
+				_container = new GameObject();
+				_container.name = "DataControll";
+				_instance = _container.AddComponent(typeof(DataController)) as DataController;
+			}
+			return _instance;
+		}
+	}
+	// Use this for initialization
+	public int Gold = 0;
+	
+	public int GoldPerSec = 1;
+	
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ unity-clicker-game/Assets/Script/GameController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+	public Text TextGold;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		TextGold.text = DataController.Instance.Gold.ToString();
+		StartCoroutine(StartCollectGold());
+	}
+	//코루틴 : 
+	IEnumerator StartCollectGold()
+	{
+		while (true)
+		{
+			yield return new WaitForSecondsRealtime(1f); // 1초를 쉬는것 
+			DataController.Instance.Gold += DataController.Instance.GoldPerSec;
+		}
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
